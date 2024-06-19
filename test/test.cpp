75fa4d2729f5cf2161dd1f0e52097f244c98b6f4
Nirajan Koirala <31283818+n7koirala@users.noreply.github.com>
2024-06-19 13:44:17 -0400

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -14,8 +14,8 @@ void PopulateAndEncryptDCRTPoly() {
 
     // Set CKKS parameters
     uint32_t batchSize = 16;
-    uint32_t depth = 1;
-    uint32_t scaleModSize = 25;
+    uint32_t depth = 6;
+    uint32_t scaleModSize = 45;  // plaintext modulus for some reasons
 
     CCParams<CryptoContextCKKSRNS> parameters;
     parameters.SetMultiplicativeDepth(depth);
@@ -34,6 +34,12 @@ void PopulateAndEncryptDCRTPoly() {
     cc->Enable(LEVELEDSHE);
     cc->Enable(ADVANCEDSHE);
 
+    // Output the generated parameters
+    std::cout << "p = " << cc->GetCryptoParameters()->GetPlaintextModulus() << std::endl;
+    std::cout << "n = " << cc->GetCryptoParameters()->GetElementParams()->GetCyclotomicOrder() / 2 << std::endl;
+    std::cout << "log2 q = " << log2(cc->GetCryptoParameters()->GetElementParams()->GetModulus().ConvertToDouble())
+              << std::endl;
+
     start = std::chrono::high_resolution_clock::now();
     auto keys = cc->KeyGen();
     cc->EvalMultKeyGen(keys.secretKey);
@@ -47,7 +53,8 @@ void PopulateAndEncryptDCRTPoly() {
     auto params = cc->GetCryptoParameters()->GetElementParams();
     DCRTPoly poly(params, Format::EVALUATION, true);
     poly.SetValuesToZero();
-    int aggVal = 125;
+    //std::cout << "poly.GetModulus().ConvertToInt() before ptxt: " << poly.GetModulus().ConvertToInt() << std::endl;
+    int aggVal = 140;
 
     for (size_t i = 0; i < poly.GetAllElements().size(); ++i) {
         NativePoly element = poly.GetElementAtIndex(i);
@@ -64,8 +71,11 @@ void PopulateAndEncryptDCRTPoly() {
        complexValues.emplace_back(static_cast<double>(poly.GetElementAtIndex(0)[i].ConvertToDouble()), 0.0);
     }
     complexValues.resize(batchSize);
+    //std::cout << "poly.GetModulus().ConvertToInt() after ptxt: " << poly.GetModulus().ConvertToInt() << std::endl;
 
     Plaintext ptxt = cc->MakeCKKSPackedPlaintext(complexValues);
+    //std::cout << "ptxt->GetEncodingParams()->GetPlaintextModulus(): " << ptxt->GetEncodingParams()->GetPlaintextModulus() << std::endl;
+
 
     // Encrypt the Plaintext object
     start = std::chrono::high_resolution_clock::now();
@@ -74,16 +84,38 @@ void PopulateAndEncryptDCRTPoly() {
     std::chrono::duration<double> encryptionTime = end - start;
     std::cout << "Encryption time: " << encryptionTime.count() << " seconds" << std::endl;
 
+    // Chebyshev approximation for ReLU function
+    // when threshold is 120, anything > 40 results in positive. So 80 difference
+    double lowerBound = 0;
+    double upperBound = 150;
+    double threshold = 138;  // set this to about 2 points below the actual value
+    uint32_t polyDegree = 27; // Degree of the polynomial for approximation
+    auto reluApprox = cc->EvalChebyshevFunction([&threshold](double x) -> double { return std::max(threshold, x); }, ciphertext, lowerBound, upperBound, polyDegree);
+ 
     // Decrypt and display the result
     Plaintext result;
     start = std::chrono::high_resolution_clock::now();
-    cc->Decrypt(ciphertext, keys.secretKey, &result);
+    cc->Decrypt(reluApprox, keys.secretKey, &result);
     end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> decryptionTime = end - start;
     std::cout << "Decryption time: " << decryptionTime.count() << " seconds" << std::endl;
 
-    result->SetLength(50);
+    result->SetLength(batchSize);
     std::cout << "Decrypted result: " << result << std::endl;
+
+    std::vector<double> vec_result = result->GetRealPackedValue();
+
+    std::cout << "Threshold value: " << threshold << std::endl;
+    std::cout << "Validating if aggregation (first slot value) crossed the threshold: " << std::endl;
+
+    if(int(vec_result[0]) > int(threshold)){
+        std::cout << "True!" <<std::endl;  
+    }
+    else{
+        std::cout << "False!" <<std::endl;  
+    }
+    
+    std::cout << "\n" <<std::endl;
 
 }
 
